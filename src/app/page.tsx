1da521f60eb750c95dc94ceda4f2cd40a5744f83
Valdenor <vfval09@gmail.com>
2025-02-08 00:22:56 -0300

    Fixed all

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,38 @@
+interface UlProps {
+  children: React.ReactNode[];
+  className: string;
+}
+
 export default function Home() {
+  const Ul = ({ children, className }: UlProps) => {
+    return (
+      <ul className={className}>
+        {children.map((li, index) => (
+          <li key={`li-${index}`}>
+            <img key={`img-${index}`} src="/line1.svg" />
+            {li}
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <main>
-      <h1>Hello world</h1>
+      <header className="p-28">
+        <div className="flex flex-col w-[317px]">
+          <h1 className="text-[64px] w-80 font-semibold">Valdenor</h1>
+          <p className="text-[15px] font-medium">
+            Front End com experiência crescente e foco em resultados
+          </p>
+        </div>
+        <nav>
+          <Ul
+            children={["Sobre", "Projetos", "Experiência"]}
+            className="opacity-50 uppercase"
+          />
+        </nav>
+      </header>
     </main>
   );
 }
