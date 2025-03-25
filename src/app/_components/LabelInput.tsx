05edd0a8a09b88371fbbb8beff8a1aba4ad1e88d
Valdenor <vfval09@gmail.com>
2025-03-25 20:31:35 -0300

    Fix unused imports from LabelInput.tsx

--- src/app/_components/LabelInput.tsx
+++ src/app/_components/LabelInput.tsx
@@ -1,5 +1,4 @@
-import { FieldErrors, UseFormRegister } from "react-hook-form";
-import ContactmeInputs, { LabelInputProps } from "../types/ContactmeInputs";
+import { LabelInputProps } from "../types/ContactmeInputs";
 import { IoAlertCircleOutline } from "react-icons/io5";
 import { HTMLInputTypeAttribute, useRef } from "react";
 
