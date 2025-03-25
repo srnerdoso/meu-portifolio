3303eb5aefc5d055671c3bc3568406a5394766ed
Valdenor <vfval09@gmail.com>
2025-03-25 20:43:25 -0300

    Fix issue "react-hooks/rules-of-hooks"

--- src/app/_components/LabelInput.tsx
+++ src/app/_components/LabelInput.tsx
@@ -1,6 +1,5 @@
 import { LabelInputProps } from "../types/ContactmeInputs";
 import { IoAlertCircleOutline } from "react-icons/io5";
-import { HTMLInputTypeAttribute, useRef } from "react";
 
 export default function LabelInput({
   type,
@@ -8,48 +7,54 @@ export default function LabelInput({
   register,
   errors,
 }: LabelInputProps) {
-  if (!register) return;
-
-  const inputRef = useRef<React.ReactElement>(null);
+  if (!register) return null;
 
   const defaultInputClassName =
     "w-full h-full text-[16px] font-normal border-[1px] px-2 rounded-md hover:border-link hover:transition hover:ease-in-out hover:duration-300 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-link flex text-black";
 
-  const setInput = (inputType: HTMLInputTypeAttribute) => {
-    inputRef.current = (
-      <input
-        type={inputType}
-        className={defaultInputClassName + " py-1"}
-        {...register(type, { required: true })}
-      />
-    );
-  };
+  const input = () => {
+    switch (type) {
+      case "subject":
+        return (
+          <input
+            type="text"
+            className={defaultInputClassName + " py-1"}
+            {...register(type, { required: true })}
+          />
+        );
 
-  switch (type) {
-    case "subject":
-      setInput("text");
-      break;
-    case "name":
-      setInput("text");
-      break;
-    case "email":
-      setInput("email");
-      break;
-    case "message":
-      inputRef.current = (
-        <textarea
-          className={defaultInputClassName + " resize-none min-h-[30vh] py-3"}
-          {...register(type, { required: true })}
-        />
-      );
-      break;
-  }
+      case "name":
+        return (
+          <input
+            type="text"
+            className={defaultInputClassName + " py-1"}
+            {...register(type, { required: true })}
+          />
+        );
+
+      case "email":
+        return (
+          <input
+            type="text"
+            className={defaultInputClassName + " py-1"}
+            {...register(type, { required: true })}
+          />
+        );
+      case "message":
+        return (
+          <textarea
+            className={defaultInputClassName + " resize-none min-h-[30vh] py-3"}
+            {...register(type, { required: true })}
+          />
+        );
+    }
+  };
 
   return (
     <div className="flex flex-col w-full gap-2">
       <label className="text-[16px] font-normal flex flex-col gap-1 w-full">
         {children}
-        {inputRef.current}
+        {input()}
       </label>
       {errors?.[type] && (
         <span className="text-red-500 flex gap-1">
